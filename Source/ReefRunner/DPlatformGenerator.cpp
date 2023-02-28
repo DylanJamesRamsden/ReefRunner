43db37f8873c5d5a3f1c594bc4c8f5f8b4302047
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2023-02-28 16:15:34 +0200

    Wrote simple platform spawning logic.

--- Source/ReefRunner/DPlatformGenerator.cpp
+++ Source/ReefRunner/DPlatformGenerator.cpp
@@ -3,19 +3,52 @@
 
 #include "DPlatformGenerator.h"
 
+#include "Engine/StaticMeshActor.h"
+
 // Sets default values
 ADPlatformGenerator::ADPlatformGenerator()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	
 }
 
 // Called when the game starts or when spawned
 void ADPlatformGenerator::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	const FVector ActorLocation = GetActorLocation();
+
+	for (int PlatformID = 0; PlatformID < NumOfStartingPlatforms; PlatformID++) // X
+	{
+		SpawnOrigin.X = ActorLocation.X + (PlatformID * SegmentWidth);
+		// Resets the Y axis origin point
+		SpawnOrigin.Y = ActorLocation.Y + (((NumOfSegmentsInPlatform - 1)/2) * SegmentWidth);
+
+		DrawDebugBox(GetWorld(), GetActorLocation(), FVector(25,25,25), FColor::Yellow, true, -1, 0, 10);
+		DrawDebugBox(GetWorld(), SpawnOrigin, FVector(25,25,25), FColor::Red, true, -1, 0, 10);
+
+		SpawnPlatform(SpawnOrigin);
+	}
+}
+
+void ADPlatformGenerator::SpawnPlatform(FVector PlatformOrigin)
+{
+	for (int SegmentID = 0; SegmentID < NumOfSegmentsInPlatform; SegmentID++) // Y
+	{
+		DrawDebugBox(GetWorld(), FVector(PlatformOrigin), FVector(50,50,50), FColor::Green, true, -1, 0, 10);
+			
+		AStaticMeshActor* MyNewActor = GetWorld()->SpawnActor<AStaticMeshActor>(AStaticMeshActor::StaticClass());
+		MyNewActor->SetMobility(EComponentMobility::Movable);
+		MyNewActor->SetActorLocation(PlatformOrigin);
+		if (UStaticMeshComponent* MeshComp = MyNewActor->GetStaticMeshComponent())
+		{
+			MeshComp->SetStaticMesh(PlatformMesh);
+		}
+
+		PlatformOrigin.Y = PlatformOrigin.Y - SegmentWidth;
+	}
 }
 
 // Called every frame
--- Source/ReefRunner/DPlatformGenerator.h
+++ Source/ReefRunner/DPlatformGenerator.h
@@ -16,8 +16,33 @@ public:
 	ADPlatformGenerator();
 
 protected:
+
+	// The number of platforms that is spawned initially at game start
+	UPROPERTY(EditDefaultsOnly)
+	int NumOfStartingPlatforms = 20;
+
+	// How many individual static meshes make up a platform
+	// @TODO It would be fun to randomize this
+	UPROPERTY(EditDefaultsOnly)
+	int NumOfSegmentsInPlatform = 7;
+
+	// How large a platform segment is (in width)
+	// Using generic UE cube for now, so they are 1m (100.0cm)
+	UPROPERTY(EditDefaultsOnly)
+	float SegmentWidth = 100.0f;
+
+	// Where the first segment of a platform spawns
+	FVector SpawnOrigin;
+
+	bool SpawningFromRight = true;
+
+	UPROPERTY(EditDefaultsOnly)
+	UStaticMesh* PlatformMesh;
+	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	virtual void SpawnPlatform(FVector PlatformOrigin);
 
 public:	
 	// Called every frame
