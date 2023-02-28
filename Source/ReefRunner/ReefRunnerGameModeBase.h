b5eeae0a827d8ad24201ed7403488048351551ac
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2023-02-28 10:50:33 +0200

    Created UE5 Project

---
+++ Source/ReefRunner/ReefRunnerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ReefRunnerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REEFRUNNER_API AReefRunnerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
