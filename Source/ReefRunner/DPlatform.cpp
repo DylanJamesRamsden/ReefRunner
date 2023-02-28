71aa5346d9067d399515f92feac56a59fd31da0f
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2023-02-28 22:44:54 +0200

    DPlatform now spawns and handles its own segments

--- Source/ReefRunner/DPlatform.cpp
+++ Source/ReefRunner/DPlatform.cpp
@@ -3,6 +3,8 @@
 
 #include "DPlatform.h"
 
+#include "DPlatformSegment.h"
+
 // Sets default values
 ADPlatform::ADPlatform()
 {
@@ -18,10 +20,41 @@ void ADPlatform::BeginPlay()
 	
 }
 
+void ADPlatform::SpawnSegment()
+{
+	DrawDebugBox(GetWorld(), FVector(SpawnOrigin), FVector(50,50,50), FColor::Green, true, -1, 0, 10);
+			
+	ADPlatformSegment* NewSegment = GetWorld()->SpawnActor<ADPlatformSegment>(SegmentTemplate);
+	NewSegment->SetMobility(EComponentMobility::Movable);
+	NewSegment->GetStaticMeshComponent()->SetMobility(EComponentMobility::Movable);
+	NewSegment->SetActorLocation(SpawnOrigin);
+
+	SpawnOrigin.Y = SpawnOrigin.Y - NewSegment->SegmentWidth;
+
+	Segments.Add(NewSegment);
+
+	if (Segments.Num() == NumSegmentsToSpawn)
+	{
+		if (GetWorldTimerManager().TimerExists(SegmentSpawnTimerHandle))
+		{
+			GetWorldTimerManager().ClearTimer(SegmentSpawnTimerHandle);
+		}	
+	}
+}
+
 // Called every frame
 void ADPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void ADPlatform::StartSegmentSpawning(FVector Origin)
+{
+	SpawnOrigin = Origin;
+	
+	SpawnSegment();
+
+	GetWorldTimerManager().SetTimer(SegmentSpawnTimerHandle, this, &ADPlatform::SpawnSegment, SegmentSpawnTime, true);
 }
 
--- Source/ReefRunner/DPlatform.h
+++ Source/ReefRunner/DPlatform.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "DPlatform.generated.h"
 
+class ADPlatformSegment;
+
 UCLASS()
 class REEFRUNNER_API ADPlatform : public AActor
 {
@@ -16,11 +18,36 @@ public:
 	ADPlatform();
 
 protected:
+
+	// How long it takes for a segment to spawn
+	UPROPERTY(EditDefaultsOnly)
+	float SegmentSpawnTime;
+
+	FTimerHandle SegmentSpawnTimerHandle;
+
+	// The segment template to spawn
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<ADPlatformSegment> SegmentTemplate;
+
+	FVector SpawnOrigin;
+
+	// How many individual static meshes make up a platform
+	// @TODO It would be fun to randomize this
+	UPROPERTY(EditDefaultsOnly, meta = (ClampMin = "1.0", ClampMax = "10.0"))
+	int NumSegmentsToSpawn;
+
+	UPROPERTY()
+	TArray<ADPlatformSegment*> Segments;
+	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void SpawnSegment();
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
+	
+	void StartSegmentSpawning(FVector Origin);
 };
