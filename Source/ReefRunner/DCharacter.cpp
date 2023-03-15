cf246352d84c3ed812ad7409c6e3d4fcea1c6f31
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2023-03-15 14:43:06 +0200

    Created DCharacter

---
+++ Source/ReefRunner/DCharacter.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+// Sets default values
+ADCharacter::ADCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+
+	SpringArmComp->SetupAttachment(RootComponent);
+	CameraComp->SetupAttachment(SpringArmComp);
+
+	SpringArmComp->bEnableCameraLag;
+}
+
+// Called when the game starts or when spawned
+void ADCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ADCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ADCharacter::PossessedBy(AController* NewController)
+{
+
+
+	Super::PossessedBy(NewController);
+
+	if (APlayerController* PC = Cast<APlayerController>(NewController))
+	{
+		PC->SetViewTarget(this);
+	}
+}
+
