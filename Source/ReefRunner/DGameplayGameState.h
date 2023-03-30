f5a3ee791c60738900dfa95568b875b7580bf4bb
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2023-03-30 23:36:42 +0200

    Created DGameplayGameState

---
+++ Source/ReefRunner/DGameplayGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "DGameplayGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REEFRUNNER_API ADGameplayGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
