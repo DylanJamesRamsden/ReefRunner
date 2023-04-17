c039e21b7342429d8de100efef18e2ab9d97d1d6
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2023-04-18 00:15:16 +0200

    Created DGameplayHUD

---
+++ Source/ReefRunner/DGameplayHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "DGameplayHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REEFRUNNER_API ADGameplayHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
