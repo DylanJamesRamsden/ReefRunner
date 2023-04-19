fe00b5f2375be99cbea1b1979644e2c46c99c6c0
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2023-04-19 19:20:57 +0200

    Created DGameplayStatics

---
+++ Source/ReefRunner/DGameplayStatics.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/GameplayStatics.h"
+#include "DGameplayStatics.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REEFRUNNER_API UDGameplayStatics : public UGameplayStatics
+{
+	GENERATED_BODY()
+	
+};
